e95e8279ef7a642797c3e5fedac47c7a65358a3f
Vedant Puri <vedantpuri@gmail.com>
2024-05-13 20:22:31 -0400

    configure BLAS

--- examples/eg1.jl
+++ examples/eg1.jl
@@ -13,6 +13,13 @@ using Zygote, Lux, ComponentArrays
 using LuxDeviceUtils, CUDA, LuxCUDA
 using BenchmarkTools
 
+# configure BLAS
+ncores = min(Sys.CPU_THREADS, length(Sys.cpu_info()))
+BLAS.set_num_threads(ncores)
+
+# configure CUDA
+CUDA.allowscalar(false)
+
 rng = Random.default_rng()
 Random.seed!(rng, 0)
 device = Lux.gpu_device()
